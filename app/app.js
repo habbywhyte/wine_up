125818e3e939c4ef02a7fcdb848924903e0381c7
Leland Jordon <lelandjordon@mac.com>
2016-05-09 15:29:39 -0400

    Added some dummy data but don't know if I need a controller to view it.

--- app/app.js
+++ app/app.js
@@ -1,5 +1,11 @@
 "use strict";
 
+var dummy = [
+  {name: "hot dog", photo_url: "dog_photo"},
+  {name: "burger", photo_url: "burger_pic"}
+];
+
+
 (function(){
   angular
   .module("food", ["ui.router"])
@@ -18,7 +24,11 @@
     .state("foodHome", {
       url: '/foods',
       templateUrl: "app/foods/foods.html",
+      // controller: "FoodIndexController",
+      // controllerAs: "showVm"
     })
+
+
 
   //
   //   .state("foodIndex", {
