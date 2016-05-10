51b8651d802869f2e5ab718fd143daad4d4506c3
Matt <mvaden08@gmail.com>
2016-05-10 19:06:14 -0400

    Added controller and views for stateparams in app.js.

--- app/app.js
+++ app/app.js
@@ -22,7 +22,26 @@
       templateUrl: "app/foods/foods.html",
       controller: "FoodIndexController",
       controllerAs: "showVm"
+       views: {
+          '':{ templateUrls: 'index.html'},
+          'foods' : { templateUrl: 'views/foods.html'},
+          'wine' : { templateUrl: 'views/wine.html'},
+      },
+      params: {
+          'food_1': {value: null}, //set value it default to null if not passed
+          'food_2': {value: null},
+          'food_3': {value: null},
+      },
+      controller: function($scope, $stateParams) {
+          //pass URL
+          $scope.name = $stateParams.name;
+          //pass data for artist page
+          $scope.food_1 = $stateParams.food_1;
+          $scope.food_2 = $stateParams.food_2;
+          $scope.food_3 = $stateParams.food_3;
+      }
     })
+  });
 
 
 
