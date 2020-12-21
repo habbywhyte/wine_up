a01f70aa8cbc436d67b6c914c6c97dd655b3c68d
Simo Oumaandi <60591478+simo-oumaandi@users.noreply.github.com>
2020-12-21 01:03:19 +0000

    Add Js Code
    Add js code to button go to home

--- app/app.js
+++ app/app.js
@@ -29,3 +29,34 @@
 
 
   })();
+document.getElementById('my-btn').onclick = function() {
+    scrollTo(document.documentElement, 0, 500);
+}
+
+function scrollTo(element, to, duration) {
+    var start = element.scrollTop,
+        change = to - start,
+        currentTime = 0,
+        increment = 20;
+
+    var animateScroll = function() {
+        currentTime += increment;
+        var val = Math.easeInOutQuad(currentTime, start, change, duration);
+        element.scrollTop = val;
+        if (currentTime < duration) {
+            setTimeout(animateScroll, increment);
+        }
+    };
+    animateScroll();
+}
+
+//t = current time
+//b = start value
+//c = change in value
+//d = duration
+Math.easeInOutQuad = function(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+};
